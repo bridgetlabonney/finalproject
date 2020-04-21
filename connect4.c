2dcd1db4b14409220810f150e5378f8c97fa4854
Marissa <62316313+marmarkaroo@users.noreply.github.com>
2020-04-21 13:08:05 -0700

    Update connect4.c

--- connect4.c
+++ connect4.c
@@ -5,3 +5,28 @@
 //
 
 #include <stdio.h>
+
+int main(){
+  int option;
+  switch(option){
+    case 0:
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    default:
+        printf("Please enter a valid option!\n");
+      break;
+  return 0;
+}
+
+int getMenuChoice(){
+  int input;
+  printf("***CONNECT 4***\n");
+  printf("1. Play Game\n2. Show Scores\n 0. EXIT\n");
+  printf("Please enter your
+  scanf("%d", &input);
+         return input;
+  }
+  
